35eb14a36e2362e29cd1e53ebdbf2e0866f6135f
SIDDHU-23052006 <bsiddharth100@gmail.com>
2026-03-10 15:04:24 +0530

    SIDDHARTH B

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,12 @@
-import app from "../server/index";
+import express from "express";
 import serverless from "serverless-http";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+await registerRoutes(app);
 
 export default serverless(app);
